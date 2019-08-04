45a6223b70a2e7b893d573bf2309085a4aafe520
Mustafa Kaya <mustafakayauta@gmail.com>
2019-08-04 16:40:13 -0500

    Mustafa commit123

--- src/test/java/com/BriteERP/pages/CRMPage.java
+++ src/test/java/com/BriteERP/pages/CRMPage.java
@@ -2,7 +2,7 @@ package com.BriteERP.pages;
 
 public class CRMPage {
     public static void main(String[] args) {
-        System.out.println("mustafakaya123");
+        System.out.println("mustafakaya12345");
     }
 
 }
